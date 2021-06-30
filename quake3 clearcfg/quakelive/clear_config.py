3e525135e1d103405abfdef8daaf10683b9c4c17
Viktor <viktpost@gmail.com>
2021-06-30 13:19:29 +0500

    Upgrade clear_config.py

--- quake3 clearcfg/quakelive/clear_config.py
+++ quake3 clearcfg/quakelive/clear_config.py
@@ -5,21 +5,17 @@ PATH_HOME = os.path.abspath(os.path.dirname(sys.argv[0]))
 os.chdir(PATH_HOME)
 os.system('cls')
 
-cfg1  = fr'{PATH_HOME}\qzconfig.cfg'
-cfg1_ = fr'{PATH_HOME}\qzconfig.cfg'
-cfg2  = fr'{PATH_HOME}\repconfig.cfg'
-cfg2_ = fr'{PATH_HOME}\repconfig.cfg'
-ql_defaults_cvars_console_dump_file_name = fr'{PATH_HOME}\defaults.ql'
+cvars_console_dump = 'defaults.ql'
 
 cvar = namedtuple('cvar', 'S,U,R,I,A,L,C,T, name, value')
 
-with open(ql_defaults_cvars_console_dump_file_name) as f:
+with open(f'{PATH_HOME}\\{cvars_console_dump}') as f:
    con_dump = f.readlines()
 
 cvars = []
-for l in con_dump:
-    flags = l[0:7]
-    name_value = l[8:].strip(' \n').split(' ', 1)
+for ln in con_dump:
+    flags = ln[0:7]
+    name_value = ln[8:].strip(' \n').split(' ', 1)
     name = name_value[0].strip()
     value = name_value[1].strip()
     if value.strip('"').startswith('0x'):
@@ -40,47 +36,74 @@ for l in con_dump:
     cvars.append(c)
 
 def remve_default_values_from_cfg(cvars, cfg, cfg_out):
-    with open(cfg) as f:
+    with open(f'{PATH_HOME}\\{cfg}') as f:
         cfg_lines = f.readlines()
     
     cfg_cvars = []
-    max_str_len = 0
-    with open(cfg_out, 'w') as f:
-        
+    cvar_max_len = 0
+    cfg_binds = []
+    bind_max_len = 0
+    with open(f'{PATH_HOME}\\{cfg_out}', 'w') as f:   
         for ln in cfg_lines:
-            l = ln.strip(' \n')
-            if not l.startswith('seta '):
-                f.write(ln)
-            else:
-                name_value = l[5:].strip().split(' ', 1)
+            ln = ln.strip(' \n')
+            if ln.startswith('seta '):
+                name_value = ln[5:].strip().split(' ', 1)
                 name = name_value[0].strip()
                 value = name_value[1].strip()
-                if name.startswith('ui_'):
-                    continue
                 if value.strip('"').startswith('0x'):
                     value  = value.lower()
-                val_pass = False
-                val_def = False
+                skip = False
+                deflt = False
+                found = False
                 for v in cvars:
                     if v.name == name:
-                        if v.R or v.I or v.C:
-                            val_pass = True
+                        found = True
+                        if v.R or v.I or v.C or name.startswith('ui_'):
+                            skip = True
                             break
                         if v.value == value:
-                            val_def = True
+                            deflt = True
                             break
-                if val_pass or val_def:
+                if skip or deflt:
                     continue
-                else:
+                elif found:
                     cfg_cvars.append((name, value))
-                    if max_str_len < len(name):
-                        max_str_len = len(name)
-        
-        for n, v in sorted(cfg_cvars, key=lambda x: x[0]):
-            sp = ' ' * (max_str_len - len(n))
+                    if cvar_max_len < len(name):
+                        cvar_max_len = len(name)
+                    continue
+            if ln.startswith('bind '):
+                name_value = ln[5:].strip().split(' ', 1)
+                name = name_value[0].strip()
+                value = name_value[1].strip()
+                cfg_binds.append((name, value))
+                if bind_max_len < len(name):
+                    bind_max_len = len(name)
+                continue
+            f.write(ln + '\n')
+
+        for n, v in sorted(cfg_binds, key=lambda x: x[1].strip('"')):
+            sp = bind_max_len - len(n)
+            if sp % 2:
+                sr = sp // 2
+                sl = sp - sr
+            else:
+                sl = sp // 2
+                sr = sp - sl
+            sp *= ' '
+            sl *= ' '
+            sr *= ' '
+            if not ';' in v:
+                v = ' ' + v.strip('"')
+            f.write(f'bind {n}{sp} {v}\n')
+
+        for n, v in sorted(cfg_cvars, key=lambda x: x[0].strip('"')):
+            sp = ' ' * (cvar_max_len - len(n))
+            if not ' ' in v and v != '""':
+                v = ' ' + v.strip('"')
             f.write(f'seta {n} {sp}{v}\n')
+
         
         f.close()
 
-remve_default_values_from_cfg(cvars, cfg1, cfg1_)
-remve_default_values_from_cfg(cvars, cfg2, cfg2_)
+remve_default_values_from_cfg(cvars, 'qzconfig.cfg', 'qzconfig.cfg')
+remve_default_values_from_cfg(cvars, 'repconfig.cfg', 'repconfig.cfg')
