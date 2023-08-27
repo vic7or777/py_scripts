2cf764596e1dec92e549c55f69077a1cb09e4aae
Viktor <viktpost@gmail.com>
2023-08-27 22:50:08 +0500

    q2 3.20 was 3.18, change to 3.18, add 3.21

---
+++ quake2 cvars/src/convert_cr_lf.py
@@ -0,0 +1,45 @@
+import sys, os
+import os.path as op
+from glob import glob
+
+skip_ext = (
+    '.aps',
+    '.bce',
+    '.bcp',
+    '.gif',
+    '.jpg',
+    '.ico',
+    '.iconheader',
+    '.o',
+    '.opt',
+    '.ssv',
+    '.tiff',
+)
+
+script_path = op.abspath(sys.argv[0])
+script_name = op.basename(script_path)
+run_path = op.dirname(script_path)
+
+print(' go')
+os.chdir(run_path)
+
+files = glob(f'{run_path}/**/*', recursive=True)
+
+LINEBREAK = '\n' # \r\n
+
+for file in files:
+
+    if op.isdir(file):
+        continue
+
+    if any(map(file.endswith, skip_ext)):
+        continue
+
+    with open(file) as f:
+        s = f.read().splitlines()
+        s = LINEBREAK.join(s)
+
+    with open(file, 'w') as f:
+        f.write(s + LINEBREAK)
+
+print(' ok')
